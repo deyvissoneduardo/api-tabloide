45760645ec344b06793c48302fc2409627c1eee3
DeyvissonEduardo <deyvissoneduardo22@gmail.com>
2026-08-26 09:32:37 -0300

    init

---
+++ src/main/java/com/tabloide/api/ApiTabloidApplication.java
@@ -0,0 +1,13 @@
+package com.tabloide.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTabloidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTabloidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabloide/api/ApiTabloidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabloide.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTabloidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
